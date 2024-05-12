9d1f4724a0405de4a658586e2434685ec1c082f5
ImranDesktop <imranthelegend12@gmail.com>
2024-05-12 15:34:07 +0600

    added

--- java/File_creating/app/build.gradle.kts
+++ java/File_creating/app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
     defaultConfig {
         applicationId = "com.example.file_creating"
         minSdk = 31
-        targetSdk = 34
+        targetSdk = 28
         versionCode = 1
         versionName = "1.0"
 
