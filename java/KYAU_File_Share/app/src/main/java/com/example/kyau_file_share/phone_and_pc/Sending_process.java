a73ebb690be5406bbbc79341a18e708356a50075
ImranDesktop <imranthelegend12@gmail.com>
2024-05-22 20:11:49 +0600

    added

--- java/KYAU_File_Share/app/src/main/java/com/example/kyau_file_share/phone_and_pc/Sending_process.java
+++ java/KYAU_File_Share/app/src/main/java/com/example/kyau_file_share/phone_and_pc/Sending_process.java
@@ -1,12 +1,16 @@
 package com.example.kyau_file_share.phone_and_pc;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.activity.OnBackPressedCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -16,12 +20,17 @@ import com.example.kyau_file_share.R;
 import com.example.kyau_file_share.Singleton;
 
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Sending_process extends AppCompatActivity {
 
     private Socket socket;
     private Button button8;
     private Button button9;
+    private TextView output;
+    private ActivityResultLauncher<Intent> filePickerLauncher;
+    private List<Uri> uris;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,13 +56,52 @@ public class Sending_process extends AppCompatActivity {
                    Toast.makeText(Sending_process.this, "Disconnected", Toast.LENGTH_SHORT).show();
             }
         });
+
         // variables initialization
         socket = Singleton.socket;
         button8 = findViewById(R.id.button8);
         button9 = findViewById(R.id.button9);
+        output = findViewById(R.id.output);
+        uris = new ArrayList<>();
+        filePickerLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), result -> {
+            if (result.getResultCode() == RESULT_OK) {
+                if (result.getData() != null) {
+                    // Get the URI(s) of the selected file(s)
+//                    uris = new ArrayList<>();
+                    if (result.getData().getClipData() != null) {
+                        int count = result.getData().getClipData().getItemCount();
+                        for (int i = 0; i < count; i++) {
+                            uris.add(result.getData().getClipData().getItemAt(i).getUri());
+                        }
+                    } else if (result.getData().getData() != null) {
+                        uris.add(result.getData().getData());
+                    }
+                    for(Uri uri:uris)
+                        output.append(uri.toString()+" \n");
+                }else{
+                    Toast.makeText(Sending_process.this, "No file selected", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+        });
+
 
         button8.setOnClickListener(v -> {
             getOnBackPressedDispatcher().onBackPressed();
         });
+
+        button9.setOnClickListener(v -> {
+            openFilePicker();
+        });
+    } // onCreate
+
+
+    // Functions initialization
+    private void openFilePicker() {
+        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        intent.setType("*/*");
+        intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true); // Allow multiple file selection
+        filePickerLauncher.launch(intent);
     }
 }
